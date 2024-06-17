a432a29a32218e5c51eadf998569347a51f8e2d1
SebastianPinoB <se.pinob@duocuc.cl>
2024-06-17 19:06:50 -0400

    Lista de edades

--- Archivos/Resuelto1.py
+++ Archivos/Resuelto1.py
@@ -1,1 +1,21 @@
-print("Hola mundo")
+import csv
+
+
+with open("datos.csv", "r") as archivo:
+    datos = csv.DictReader(archivo)
+    mayores = []
+    menores = []
+
+    for i in datos:
+        if int(i["Edad"]) >= 18:
+            mayores.append(i)
+        else:
+            menores.append(i)
+
+    print("Mayores")
+    for i in mayores:
+        print(i)
+
+    print("Menores")
+    for i in menores:
+        print(i)
